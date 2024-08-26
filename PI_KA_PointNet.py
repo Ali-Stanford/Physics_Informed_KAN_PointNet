28d38bbd08f9d3cf73a1a312dcd4a17d63ff2d6b
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2024-08-26 01:47:04 -0700

    Update PI_KA_PointNet.py

--- PI_KA_PointNet.py
+++ PI_KA_PointNet.py
@@ -19,13 +19,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn.init as init
 import time
-
+import gc
 ###############
 
 if torch.cuda.is_available():
     device = torch.device("cuda")  # GPU available
 else:
     device = torch.device("cpu")  # Only CPU available
+
+###############
+
+gc.collect()
+torch.cuda.empty_cache()
 
 ###############
 
@@ -36,9 +41,9 @@ outter_surface_n = 492 # number of points on the outter surface
 
 SCALE = 1.0
 Learning_rate = 0.0005 #0.001
-Epoch = 2 #10000
-Nb = 1 #batch size, works for: 2, 5, 10
-J_Loss = 0.0050
+Epoch = 1000 
+Nb = 7 # works unitl 7 (memory sensetiv)
+J_Loss = 0.0350
 
 ###############
 
@@ -48,7 +53,7 @@ kappa =  np.power(2.0,1.5)*np.power(10.0,-2.5)
 
 ###############
 
-poly_degree = 3 #5
+poly_degree = 3
 ALPHA = 1.0
 BETA = 1.0
 
@@ -61,7 +66,7 @@ interior_list = [] #interior nodes without full, BC, sparse
 ###############
 
 Data = np.load('/scratch/users/kashefi/Model10/ZYkanpipn1/Data.npy')
-Data = Data[:1, :, :]
+#Data = Data[:134, :, :]
 
 data, num_points, _ = Data.shape
 
@@ -519,7 +524,7 @@ def computeRelativeL2OnSurface(X,Tp,index):
 ##################################
 
 def compute_T_surface(X,Tp,index):
-
+    
     Nu_con = 0
     for i in range(N_boundary):
         if np.sqrt(np.square(0.0*np.pi-X[index][i][0]) + np.square(0.0*np.pi-X[index][i][1])) > (0.8):
@@ -553,6 +558,82 @@ problemSet_nonuniform_grid()
 
 ##################################
 
+def TheLossEfficient(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose_BC_p,pose_sparse_p,pose_interior_p):
+
+    Y = model(X)
+
+    pose_BC = torch.tensor(pose_BC, dtype=torch.long, device=Y.device)
+    pose_sparse= torch.tensor(pose_sparse, dtype=torch.long, device=Y.device)
+    pose_interior = torch.tensor(pose_interior, dtype=torch.long, device=Y.device)
+    pose_BC_temperature_p = torch.tensor(pose_BC_temperature_p, dtype=torch.long, device=Y.device)
+    pose_BC_p = torch.tensor(pose_BC_p, dtype=torch.long, device=Y.device)
+    pose_sparse_p = torch.tensor(pose_sparse_p, dtype=torch.long, device=Y.device)
+    pose_interior_p = torch.tensor(pose_interior_p, dtype=torch.long, device=Y.device)
+
+    u_in = Y[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    v_in = Y[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    T_in = Y[:, 3, :].contiguous().view(-1)[pose_interior_p]
+
+    u_boundary = Y[:, 0, :].contiguous().view(-1)[pose_BC_p]
+    v_boundary = Y[:, 1, :].contiguous().view(-1)[pose_BC_p]
+    T_boundary = Y[:, 3, :].contiguous().view(-1)[pose_BC_p]
+
+    u_sparse = Y[:, 0, :].contiguous().view(-1)[pose_sparse_p]
+    v_sparse = Y[:, 1, :].contiguous().view(-1)[pose_sparse_p]
+    p_sparse = Y[:, 2, :].contiguous().view(-1)[pose_sparse_p]
+    T_sparse = Y[:, 3, :].contiguous().view(-1)[pose_sparse_p]
+
+    du = torch.autograd.grad(Y[:, 0, :], X, grad_outputs=torch.ones_like(Y[:, 0, :]), create_graph=True)[0]
+    dv = torch.autograd.grad(Y[:, 1, :], X, grad_outputs=torch.ones_like(Y[:, 1, :]), create_graph=True)[0]
+    dp = torch.autograd.grad(Y[:, 2, :], X, grad_outputs=torch.ones_like(Y[:, 2, :]), create_graph=True)[0]
+    dT = torch.autograd.grad(Y[:, 3, :], X, grad_outputs=torch.ones_like(Y[:, 3, :]), create_graph=True)[0]
+    
+    du_dx_in = du[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    du_dy_in = du[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    dv_dx_in = dv[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    dv_dy_in = dv[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    dp_dx_in = dp[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    dp_dy_in = dp[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    dT_dx_in = dT[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    dT_dy_in = dT[:, 1, :].contiguous().view(-1)[pose_interior_p]
+
+    d2u_x = torch.autograd.grad(du[:, 0, :], X, grad_outputs=torch.ones_like(du[:, 0, :]), create_graph=True)[0]
+    d2u_y = torch.autograd.grad(du[:, 1, :], X, grad_outputs=torch.ones_like(du[:, 1, :]), create_graph=True)[0]    
+    
+    d2v_x = torch.autograd.grad(dv[:, 0, :], X, grad_outputs=torch.ones_like(dv[:, 0, :]), create_graph=True)[0]
+    d2v_y = torch.autograd.grad(dv[:, 1, :], X, grad_outputs=torch.ones_like(dv[:, 1, :]), create_graph=True)[0]
+
+    d2T_x = torch.autograd.grad(dT[:, 0, :], X, grad_outputs=torch.ones_like(dT[:, 0, :]), create_graph=True)[0]
+    d2T_y = torch.autograd.grad(dT[:, 1, :], X, grad_outputs=torch.ones_like(dT[:, 1, :]), create_graph=True)[0]
+
+    d2u_dx2_in = d2u_x[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    d2u_dy2_in = d2u_y[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    d2v_dx2_in = d2v_x[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    d2v_dy2_in = d2v_y[:, 1, :].contiguous().view(-1)[pose_interior_p]
+    d2T_dx2_in = d2T_x[:, 0, :].contiguous().view(-1)[pose_interior_p]
+    d2T_dy2_in = d2T_y[:, 1, :].contiguous().view(-1)[pose_interior_p]
+
+    boundary_u_truth = torch.tensor(cfd_u[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
+    boundary_v_truth = torch.tensor(cfd_v[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
+
+    sparse_u_truth = torch.tensor(cfd_u[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_v_truth = torch.tensor(cfd_v[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_T_truth = torch.tensor(cfd_T[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_p_truth = torch.tensor(cfd_p[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+
+    r1 = density*(u_in*du_dx_in + v_in*du_dy_in) + dp_dx_in - viscosity*(d2u_dx2_in + d2u_dy2_in)
+    r2 = density*(u_in*dv_dx_in + v_in*dv_dy_in) + dp_dy_in - viscosity*(d2v_dx2_in + d2v_dy2_in) - density*1.0*1.0*(T_in - 0.0)
+    r3 = du_dx_in + dv_dy_in
+    r4 = density*(u_in*dT_dx_in + v_in*dT_dy_in) - (kappa/1.0)*(d2T_dx2_in + d2T_dy2_in)
+   
+    PDE_cost = torch.mean(r1**2 + r2**2 + r3**2 + r4**2)
+    BC_cost = torch.mean((u_boundary - 0.0)**2 + (v_boundary - 0.0)**2) + torch.mean((T_boundary - 0.0)**2)
+    Sparse_cost = torch.mean((u_sparse - sparse_u_truth)**2 + (v_sparse - sparse_v_truth)**2 + (p_sparse - sparse_p_truth)**2 + (T_sparse - sparse_T_truth)**2)
+    
+    return PDE_cost + Sparse_cost + BC_cost
+
+#################################
+
 def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose_BC_p,pose_sparse_p,pose_interior_p):
 
     Y = model(X)
@@ -583,13 +664,9 @@ def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose
     p_sparse = p[pose_sparse_p]
     T_sparse = T[pose_sparse_p]
 
-    boundary_u_truth = torch.tensor(cfd_u[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
-    boundary_v_truth = torch.tensor(cfd_v[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
-
-    sparse_u_truth = torch.tensor(cfd_u[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
-    sparse_v_truth = torch.tensor(cfd_v[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
-    sparse_T_truth = torch.tensor(cfd_T[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
-    sparse_p_truth = torch.tensor(cfd_p[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    del u, v, p, T
+    gc.collect()
+    torch.cuda.empty_cache()
 
     #DYDX = torch.autograd.grad(Y, X, grad_outputs=torch.ones_like(Y), create_graph=True)[0]
 
@@ -606,6 +683,10 @@ def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose
     dp_dy = dp[:, 1, :]
     dT_dx = dT[:, 0, :]
     dT_dy = dT[:, 1, :]
+
+    del dp
+    gc.collect()
+    torch.cuda.empty_cache()
 
     du_dx = du_dx.contiguous().view(-1)
     du_dy = du_dy.contiguous().view(-1)
@@ -625,12 +706,30 @@ def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose
     dT_dx_in = dT_dx[pose_interior_p]
     dT_dy_in = dT_dy[pose_interior_p]
 
+    del du_dx, du_dy, dv_dx, dv_dy, dp_dx, dp_dy, dT_dx, dT_dy
+    gc.collect()
+    torch.cuda.empty_cache()
+
     d2u_x = torch.autograd.grad(du[:, 0, :], X, grad_outputs=torch.ones_like(du[:, 0, :]), create_graph=True)[0]
     d2u_y = torch.autograd.grad(du[:, 1, :], X, grad_outputs=torch.ones_like(du[:, 1, :]), create_graph=True)[0]    
+    
+    del du
+    gc.collect()
+    torch.cuda.empty_cache()
+
     d2v_x = torch.autograd.grad(dv[:, 0, :], X, grad_outputs=torch.ones_like(dv[:, 0, :]), create_graph=True)[0]
     d2v_y = torch.autograd.grad(dv[:, 1, :], X, grad_outputs=torch.ones_like(dv[:, 1, :]), create_graph=True)[0]
+
+    del dv
+    gc.collect()
+    torch.cuda.empty_cache()
+
     d2T_x = torch.autograd.grad(dT[:, 0, :], X, grad_outputs=torch.ones_like(dT[:, 0, :]), create_graph=True)[0]
     d2T_y = torch.autograd.grad(dT[:, 1, :], X, grad_outputs=torch.ones_like(dT[:, 1, :]), create_graph=True)[0]
+
+    del dT
+    gc.collect()
+    torch.cuda.empty_cache()
 
     d2u_dx2 = d2u_x[:, 0, :]
     d2u_dy2 = d2u_y[:, 1, :]
@@ -638,6 +737,10 @@ def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose
     d2v_dy2 = d2v_y[:, 1, :]
     d2T_dx2 = d2T_x[:, 0, :]
     d2T_dy2 = d2T_y[:, 1, :]
+
+    del d2u_x, d2u_y, d2v_x, d2v_y, d2T_x, d2T_y
+    gc.collect()
+    torch.cuda.empty_cache()
 
     d2u_dx2 = d2u_dx2.contiguous().view(-1)
     d2u_dy2 = d2u_dy2.contiguous().view(-1)
@@ -653,12 +756,28 @@ def TheLoss(model,X,pose_BC,pose_sparse,pose_interior,pose_BC_temperature_p,pose
     d2T_dx2_in = d2T_dx2[pose_interior_p]
     d2T_dy2_in = d2T_dy2[pose_interior_p]
 
+    del d2u_dx2, d2u_dy2, d2v_dx2, d2v_dy2, d2T_dx2, d2T_dy2
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    boundary_u_truth = torch.tensor(cfd_u[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
+    boundary_v_truth = torch.tensor(cfd_v[pose_BC.cpu()], dtype=torch.float32, device=Y.device)
+
+    sparse_u_truth = torch.tensor(cfd_u[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_v_truth = torch.tensor(cfd_v[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_T_truth = torch.tensor(cfd_T[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+    sparse_p_truth = torch.tensor(cfd_p[pose_sparse.cpu()], dtype=torch.float32, device=Y.device)
+
     r1 = density*(u_in*du_dx_in + v_in*du_dy_in) + dp_dx_in - viscosity*(d2u_dx2_in + d2u_dy2_in)
     r2 = density*(u_in*dv_dx_in + v_in*dv_dy_in) + dp_dy_in - viscosity*(d2v_dx2_in + d2v_dy2_in) - density*1.0*1.0*(T_in - 0.0)
     r3 = du_dx_in + dv_dy_in
     r4 = density*(u_in*dT_dx_in + v_in*dT_dy_in) - (kappa/1.0)*(d2T_dx2_in + d2T_dy2_in)
    
     PDE_cost = torch.mean(r1**2 + r2**2 + r3**2 + r4**2)
+
+    del r1, r2, r3, r4
+    torch.cuda.empty_cache()
+
     BC_cost = torch.mean((u_boundary - 0.0)**2 + (v_boundary - 0.0)**2) + torch.mean((T_boundary - 0.0)**2)
     
     Sparse_cost = torch.mean((u_sparse - sparse_u_truth)**2 + (v_sparse - sparse_v_truth)**2 + (p_sparse - sparse_p_truth)**2 + (T_sparse - sparse_T_truth)**2)
@@ -689,8 +808,10 @@ def build_model_Thermal():
         arr = np.arange(data)
         np.random.shuffle(arr)
         
-        for sb in range(int(data/Nb)):
-            pointer = arr[int(sb*Nb):int((sb+1)*Nb)]
+        for sb in range(int(np.ceil(data / Nb))):
+            start_idx = int(sb * Nb)
+            end_idx = int(min((sb + 1) * Nb, data))
+            pointer = arr[start_idx:end_idx]
 
             group_BC = np.zeros(int(len(pointer)*len(BC_list)), dtype=int)
             group_sparse = np.zeros(int(len(pointer)*sparse_n), dtype=int)
@@ -720,25 +841,29 @@ def build_model_Thermal():
             group_interior_p = np.zeros(int(len(pointer)*len(interior_list)), dtype=int)
 
             catch = 0
-            for ii in range(Nb):
+            for ii in range(len(pointer)):
+            #for ii in range(Nb):
                 for jj in range(len(BC_list)):
                     group_BC_p[catch] = int(ii*num_points + jj)
                     catch += 1
 
             catch = 0
-            for ii in range(Nb):
+            for ii in range(len(pointer)):
+            #for ii in range(Nb):
                 for jj in range(outter_surface_n):
                     group_BC_temperature_p[catch] = BC_list_temperature_inverse[pointer[ii]][jj] + ii*num_points
                     catch += 1
 
             catch = 0
-            for ii in range(Nb):
+            for ii in range(len(pointer)):
+            #for ii in range(Nb):
                 for jj in range(sparse_n):
                     group_sparse_p[catch] = sparse_list[pointer[ii]][jj] + ii*num_points
                     catch += 1
 
             catch = 0
-            for ii in range(Nb):
+            for ii in range(len(pointer)):
+            #for ii in range(Nb):
                 for jj in range(len(interior_list)):
                     group_interior_p[catch] = int(ii*num_points + len(BC_list) + jj)
                     catch += 1
@@ -751,7 +876,7 @@ def build_model_Thermal():
             optimizer.zero_grad()
             
             # Forward passs
-            cost_value = TheLoss(model, X_train_mini, group_BC,group_sparse,group_interior,group_BC_temperature_p,group_BC_p,group_sparse_p,group_interior_p)
+            cost_value = TheLossEfficient(model, X_train_mini, group_BC,group_sparse,group_interior,group_BC_temperature_p,group_BC_p,group_sparse_p,group_interior_p)
             cost_value.backward()
             optimizer.step()
             
